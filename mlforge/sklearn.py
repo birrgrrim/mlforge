87ac7ed10b9e5668fc16a20b8fb1986debc792bd
Volodymyr Gnateiko <birrgrrim@gmail.com>
2025-06-29 17:43:48 +0300

    SklearnModelWrapper -> RandomForestModelWrapper.

--- mlforge/sklearn.py
+++ mlforge/sklearn.py
@@ -1,7 +1,7 @@
 from sklearn.ensemble import RandomForestClassifier
 from .base import BaseModelWrapper
 
-class SklearnModelWrapper(BaseModelWrapper):
+class RandomForestModelWrapper(BaseModelWrapper):
     def __init__(self, hyperparameters, features):
         super().__init__(hyperparameters, features)
         self.model = RandomForestClassifier(**hyperparameters)
